15fa30fa973b956440a243b8067ef341f431e285
Rhutuja Mane <rutujamane9611@gmail.com>
2026-04-15 19:15:45 +0530

    Add array traversal using pointer

---
+++ pointer-concepts/array_traversal_pointer.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdint.h>
+
+int main() {
+    uint8_t array[5] = {10, 20, 30, 40, 50};
+    uint8_t *p = array;
+
+    for (int i = 0; i < 5; i++) {
+        printf("value %d, address %p\n", *p, p);
+        *p++;  // move to next element
+    }
+
+    return 0;
+}
