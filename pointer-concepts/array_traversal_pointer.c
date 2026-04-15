428d3695fed67811bad02f8294620043d592f3fb
Rhutuja Mane <rutujamane9611@gmail.com>
2026-04-15 19:43:55 +0530

    array traversal using pointer

--- pointer-concepts/array_traversal_pointer.c
+++ pointer-concepts/array_traversal_pointer.c
@@ -1,6 +1,6 @@
 // Program: Array Traversal using Pointer
 // Concept: Pointer Arithmetic
-// Description: This program prints values and addresses using pointer
+// Description: This program prints values and addresses using pointer 
 
 #include <stdio.h>
 #include <stdint.h>
